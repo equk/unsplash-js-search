3b4106e9a260fb6cb57ddb66a2b854ba5529c009
equk <equk@users.noreply.github.com>
2018-07-31 20:20:35 +0100

    setup a fetch request to pull in json & log to console

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,23 @@
-const API_CLIENTID = '-insert-your-api-key-'
+const API_CLIENTID = ''
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const imageSection = document.querySelector('.images');
 const API_URL = `https://api.unsplash.com/search/photos?page=1&per_page=20&client_id=${API_CLIENTID}`
+
+form.addEventListener('submit', formSubmitted);
+
+function formSubmitted(event) {
+  event.preventDefault();
+  let searchTerm = input.value;
+
+  search(searchTerm)
+}
+
+function search(searchTerm) {
+  let url = `${API_URL}&query=${searchTerm}`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result.results);
+    });
+}
